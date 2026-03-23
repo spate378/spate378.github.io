48ece21da87e569745830b4fdb9ac53f5fa72c61
spate378 <spate378@students.kennesaw.edu>
2026-03-23 15:16:39 -0400

    bookshelf.js

--- milestone2/bookshelf.js
+++ milestone2/bookshelf.js
@@ -1,44 +1,34 @@
-var volumeIDs = [
-  "Kw5sEQAAQBAJ",
-  "XfFvDwAAQBAJ",
-  "q0rCTQlvNMoC",
-  "GZXlEAAAQBAJ",
-  "mwWNEQAAQBAJ"
+var booksID = ["Kw5sEQAAQBAJ","XfFvDwAAQBAJ","q0rCTQlvNMoC","GZXlEAAAQBAJ","mwWNEQAAQBAJ"
 ];
 
-var apiKey = "AIzaSyA6lSYNLDYST73FsnRTEKXHqX7wbpi90qU";
-var loadedBooks = 0;
+var gKey = "AIzaSyA6lSYNLDYST73FsnRTEKXHqX7wbpi90qU";
+var bookNum = 0;
 
 $(document).ready(function () {
   $("#results").empty();
-  $("#summary").html("Loading " + volumeIDs.length + " books...");
-
-  volumeIDs.forEach(function (id) {
-    var apiUrl = "https://www.googleapis.com/books/v1/volumes/" + id + "?key=" + apiKey;
+  $("#summary").html("Now getting " + booksID.length + " books...");
+  booksID.forEach(function (id) {
+  var apiUrl = "https://www.googleapis.com/books/v1/volumes/" + id + "?key=" + gKey;
 
     $.getJSON(apiUrl, function (data) {
       var book = data.volumeInfo;
       var title = book.title || "No title";
-      var authors = book.authors ? book.authors.join(", ") : "N/A";
+      var bookAuthor = book.authors ? book.authors.join(", ") : "N/A";
       var img = book.imageLinks ? book.imageLinks.thumbnail : "";
-
-      var html = "<div class='book'>";
+      var bookHtml = "<div class='book'>";
+      
       if (img) {
-        html += "<img class='thumb' src='" + img + "'>";
+        bookHtml += "<img class='thumb' src='" + img + "'>";
       }
-      html += "<p class='book-title'><a href='details.html?id=" + id + "'>" + title + "</a></p>";
-      html += "<p class='book-meta'>" + authors + "</p>";
-      html += "</div>";
-
-      $("#results").append(html);
-      loadedBooks++;
-
-      if (loadedBooks === volumeIDs.length) {
-        $("#summary").html("Showing " + loadedBooks + " books from my shelf.");
-      }
-    }).fail(function () {
-      loadedBooks++;
-      $("#summary").html("<span style='color:red'>Error loading one or more books.</span>");
+      bookHtml += "<p class='book-title'><a href='details.html?id=" + id + "'>" + title + "</a></p>";
+      bookHtml += "<p class='book-meta'>" + bookAuthor + "</p>";
+      bookHtml += "</div>";
+      $("#results").append(bookHtml);
+      bookNum++;
+      
+      if (bookNum === booksID.length) {$("#summary").html("Showing " + bookNum + " books from my shelf.");}
+    }).fail(function () { bookNum++;
+      $("#summary").html("<span style='color:red'>Error, Something went wrong.</span>");
     });
-  });
+});
 });
